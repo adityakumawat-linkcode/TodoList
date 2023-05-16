0d1032e4a744028b64e03a8c192fc5928279f793
adityakumawat-linkcode <133575536+adityakumawat-linkcode@users.noreply.github.com>
2023-05-16 19:32:39 +0530

    commit

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -4,30 +4,29 @@ import { Observable } from 'rxjs';
 import { Task } from '../model/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrudService {
+  serviceURL: string;
 
-  serviceURL : string ;
-
-  constructor(private http : HttpClient) {
-    this.serviceURL = "http://localhost:3000/tasks"
+  constructor(private http: HttpClient) {
+    // this.serviceURL = 'http://localhost:3000/tasks';
+    this.serviceURL = 'https://todo-list-amber-ten.vercel.app/';
   }
 
-  addTask(task : Task) : Observable<Task> {
-    return this.http.post<Task>(this.serviceURL,task);
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.serviceURL, task);
   }
 
-  getAllTask() : Observable<Task[]> {
+  getAllTask(): Observable<Task[]> {
     return this.http.get<Task[]>(this.serviceURL);
   }
 
-  deleteTask(task : Task) : Observable<Task> {
-    return this.http.delete<Task>(this.serviceURL+'/'+task.id);
+  deleteTask(task: Task): Observable<Task> {
+    return this.http.delete<Task>(this.serviceURL + '/' + task.id);
   }
 
-  editTask(task : Task) : Observable<Task> {
-    return this.http.put<Task>(this.serviceURL+'/'+task.id,task);
+  editTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(this.serviceURL + '/' + task.id, task);
   }
-
 }
